2c49df25ed39c8499b3e9bf36ea4d8ae5a6e5a38
JordanHayen <jordan.hayen@gmail.com>
2021-10-31 20:28:42 -0700

    Milestone 3, CST150

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Milestone3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InventoryDisplay = new System.Windows.Forms.ListBox();
+            this.SearchResults = new System.Windows.Forms.ListBox();
+            this.ItemNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ItemStockTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ItemPriceTextBox = new System.Windows.Forms.TextBox();
+            this.AddItem = new System.Windows.Forms.Button();
+            this.RemoveSelected = new System.Windows.Forms.Button();
+            this.EditSelected = new System.Windows.Forms.Button();
+            this.ItemPriceEditorTextBox = new System.Windows.Forms.TextBox();
+            this.ItemStockEditorTextBox = new System.Windows.Forms.TextBox();
+            this.ItemNameEditorTextBox = new System.Windows.Forms.TextBox();
+            this.AddStockTextBox = new System.Windows.Forms.TextBox();
+            this.AddStockButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SearchBar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InventoryDisplay
+            // 
+            this.InventoryDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.InventoryDisplay.FormattingEnabled = true;
+            this.InventoryDisplay.ItemHeight = 20;
+            this.InventoryDisplay.Items.AddRange(new object[] {
+            "Item\t\tStock\t\tPrice"});
+            this.InventoryDisplay.Location = new System.Drawing.Point(12, 12);
+            this.InventoryDisplay.Name = "InventoryDisplay";
+            this.InventoryDisplay.Size = new System.Drawing.Size(388, 324);
+            this.InventoryDisplay.TabIndex = 0;
+            this.InventoryDisplay.SelectedIndexChanged += new System.EventHandler(this.InventoryDisplay_SelectedIndexChanged);
+            // 
+            // SearchResults
+            // 
+            this.SearchResults.FormattingEnabled = true;
+            this.SearchResults.Location = new System.Drawing.Point(406, 64);
+            this.SearchResults.Name = "SearchResults";
+            this.SearchResults.Size = new System.Drawing.Size(159, 95);
+            this.SearchResults.TabIndex = 5;
+            this.SearchResults.SelectedIndexChanged += new System.EventHandler(this.SearchResults_SelectedIndexChanged);
+            // 
+            // ItemNameTextBox
+            // 
+            this.ItemNameTextBox.Location = new System.Drawing.Point(170, 413);
+            this.ItemNameTextBox.Name = "ItemNameTextBox";
+            this.ItemNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ItemNameTextBox.TabIndex = 6;
+            this.ItemNameTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(170, 373);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Item Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(300, 373);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Item Stock";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ItemStockTextBox
+            // 
+            this.ItemStockTextBox.Location = new System.Drawing.Point(300, 413);
+            this.ItemStockTextBox.Name = "ItemStockTextBox";
+            this.ItemStockTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ItemStockTextBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(430, 373);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Item Price";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ItemPriceTextBox
+            // 
+            this.ItemPriceTextBox.Location = new System.Drawing.Point(430, 413);
+            this.ItemPriceTextBox.Name = "ItemPriceTextBox";
+            this.ItemPriceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ItemPriceTextBox.TabIndex = 10;
+            // 
+            // AddItem
+            // 
+            this.AddItem.Location = new System.Drawing.Point(40, 413);
+            this.AddItem.Name = "AddItem";
+            this.AddItem.Size = new System.Drawing.Size(100, 20);
+            this.AddItem.TabIndex = 12;
+            this.AddItem.Text = "Add Item";
+            this.AddItem.UseVisualStyleBackColor = true;
+            this.AddItem.Click += new System.EventHandler(this.AddItem_Click);
+            // 
+            // RemoveSelected
+            // 
+            this.RemoveSelected.Location = new System.Drawing.Point(406, 165);
+            this.RemoveSelected.Name = "RemoveSelected";
+            this.RemoveSelected.Size = new System.Drawing.Size(159, 41);
+            this.RemoveSelected.TabIndex = 13;
+            this.RemoveSelected.Text = "Remove Selected";
+            this.RemoveSelected.UseVisualStyleBackColor = true;
+            this.RemoveSelected.Visible = false;
+            this.RemoveSelected.Click += new System.EventHandler(this.RemoveSelected_Click);
+            // 
+            // EditSelected
+            // 
+            this.EditSelected.Location = new System.Drawing.Point(40, 439);
+            this.EditSelected.Name = "EditSelected";
+            this.EditSelected.Size = new System.Drawing.Size(100, 20);
+            this.EditSelected.TabIndex = 17;
+            this.EditSelected.Text = "Edit Selected";
+            this.EditSelected.UseVisualStyleBackColor = true;
+            this.EditSelected.Visible = false;
+            this.EditSelected.Click += new System.EventHandler(this.EditSelected_Click);
+            // 
+            // ItemPriceEditorTextBox
+            // 
+            this.ItemPriceEditorTextBox.Location = new System.Drawing.Point(430, 439);
+            this.ItemPriceEditorTextBox.Name = "ItemPriceEditorTextBox";
+            this.ItemPriceEditorTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ItemPriceEditorTextBox.TabIndex = 16;
+            this.ItemPriceEditorTextBox.Visible = false;
+            // 
+            // ItemStockEditorTextBox
+            // 
+            this.ItemStockEditorTextBox.Location = new System.Drawing.Point(300, 439);
+            this.ItemStockEditorTextBox.Name = "ItemStockEditorTextBox";
+            this.ItemStockEditorTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ItemStockEditorTextBox.TabIndex = 15;
+            this.ItemStockEditorTextBox.Visible = false;
+            // 
+            // ItemNameEditorTextBox
+            // 
+            this.ItemNameEditorTextBox.Location = new System.Drawing.Point(170, 439);
+            this.ItemNameEditorTextBox.Name = "ItemNameEditorTextBox";
+            this.ItemNameEditorTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ItemNameEditorTextBox.TabIndex = 14;
+            this.ItemNameEditorTextBox.Visible = false;
+            // 
+            // AddStockTextBox
+            // 
+            this.AddStockTextBox.Location = new System.Drawing.Point(560, 439);
+            this.AddStockTextBox.Name = "AddStockTextBox";
+            this.AddStockTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AddStockTextBox.TabIndex = 18;
+            this.AddStockTextBox.Visible = false;
+            // 
+            // AddStockButton
+            // 
+            this.AddStockButton.Location = new System.Drawing.Point(560, 413);
+            this.AddStockButton.Name = "AddStockButton";
+            this.AddStockButton.Size = new System.Drawing.Size(100, 20);
+            this.AddStockButton.TabIndex = 19;
+            this.AddStockButton.Text = "Add Stock";
+            this.AddStockButton.UseVisualStyleBackColor = true;
+            this.AddStockButton.Visible = false;
+            this.AddStockButton.Click += new System.EventHandler(this.AddStockButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(406, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 23);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Search";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SearchBar
+            // 
+            this.SearchBar.Location = new System.Drawing.Point(406, 38);
+            this.SearchBar.Name = "SearchBar";
+            this.SearchBar.Size = new System.Drawing.Size(159, 20);
+            this.SearchBar.TabIndex = 21;
+            this.SearchBar.TextChanged += new System.EventHandler(this.SearchBar_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 506);
+            this.Controls.Add(this.SearchBar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.AddStockButton);
+            this.Controls.Add(this.AddStockTextBox);
+            this.Controls.Add(this.EditSelected);
+            this.Controls.Add(this.ItemPriceEditorTextBox);
+            this.Controls.Add(this.ItemStockEditorTextBox);
+            this.Controls.Add(this.ItemNameEditorTextBox);
+            this.Controls.Add(this.RemoveSelected);
+            this.Controls.Add(this.AddItem);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ItemPriceTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ItemStockTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ItemNameTextBox);
+            this.Controls.Add(this.SearchResults);
+            this.Controls.Add(this.InventoryDisplay);
+            this.Name = "Form1";
+            this.Text = "InventoryManager Class Test Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox InventoryDisplay;
+        private System.Windows.Forms.ListBox SearchResults;
+        private System.Windows.Forms.TextBox ItemNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ItemStockTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ItemPriceTextBox;
+        private System.Windows.Forms.Button AddItem;
+        private System.Windows.Forms.Button RemoveSelected;
+        private System.Windows.Forms.Button EditSelected;
+        private System.Windows.Forms.TextBox ItemPriceEditorTextBox;
+        private System.Windows.Forms.TextBox ItemStockEditorTextBox;
+        private System.Windows.Forms.TextBox ItemNameEditorTextBox;
+        private System.Windows.Forms.TextBox AddStockTextBox;
+        private System.Windows.Forms.Button AddStockButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox SearchBar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Milestone3
+{
+    public partial class Form1 : Form
+    {
+        InventoryManager manager; // An InventoryManager object
+        const string LISTBOXHEADER = "Item\t\tStock\t\tPrice"; // A string constant that will contain the header for the InventoryDisplay ListBox
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // This code runs when the form is loaded. It sets the manager field to a new InventoryManager object
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            manager = new InventoryManager();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            manager.AddItem(new InventoryItem("Forks", 300, 4.99));
+            manager.AddItem(new InventoryItem("Knives", 550, 7.99));
+            manager.AddItem(new InventoryItem("Spoons", 875, 3.99));
+
+            InventoryDisplay.Items.AddRange(manager.Display().ToArray());
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // This code runs when the AddItem button is clicked. It processes data from the TextBoxes for the new item's name, price and stock, uses that information to create a new InventoryItem, adds that to the manager field, and updates the InventoryDisplay ListBox
+        private void AddItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                manager.AddItem(new InventoryItem(ItemNameTextBox.Text, int.Parse(ItemStockTextBox.Text), double.Parse(ItemPriceTextBox.Text))); // Processes data from the TextBoxes for the new item's name, price and stock and uses that information to create a new InventoryItem, then add that InventoryItem to the manager object
+                InventoryDisplay.Items.Clear(); // Clears the InventoryDisplay ListBox
+                InventoryDisplay.Items.Add(LISTBOXHEADER); // Adds the LISTBOXHEADER string to the InventoryDisplay ListBox
+                InventoryDisplay.Items.AddRange(manager.Display().ToArray()); // Calls the manager object's Display method, sets the result to an array, and adds the result to the InventoryDisplay ListBox
+            }
+            catch(Exception ex) // If any errors occur when parsing data from the TextBoxes, a message box containing the error message is displayed
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            // The TextBoxes are cleared
+            ItemNameTextBox.Text = "";
+            ItemStockTextBox.Text = "";
+            ItemPriceTextBox.Text = "";
+        }
+
+        //  This code runs when an item is selected in the InventoryDisplay ListBox. If the selected item is not the heder, then the button for removing an item is displayed, along with the button and TextBoxes used to edit an item
+        private void InventoryDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(InventoryDisplay.SelectedIndex > 0) // If an item is selected that is not the header, then the button for removing an item is displayed, along with the button and TextBoxes used to edit an item
+            {
+                RemoveSelected.Visible = true; // The button for removing a selected item is set to be visible
+                EditSelected.Visible = true; // The button for editting a selected item is set to be visible
+                ItemNameEditorTextBox.Visible = true; // The TextBox for editting an item's name is set to be visible
+                ItemNameEditorTextBox.Text = manager.Items[InventoryDisplay.SelectedIndex - 1].ItemName;
+                ItemStockEditorTextBox.Visible = true; // The textbox for editting an item's stock is set to be visible
+                ItemStockEditorTextBox.Text = manager.Items[InventoryDisplay.SelectedIndex - 1].Stock.ToString();
+                ItemPriceEditorTextBox.Visible = true; // The textbox for editting an item's price is set to be visible
+                ItemPriceEditorTextBox.Text = manager.Items[InventoryDisplay.SelectedIndex - 1].Price.ToString();
+                AddStockButton.Visible = true; // The button for adding to an item's stock is set to be visible 
+                AddStockTextBox.Visible = true; // The textbox for adding to an item's stock is set to be visible
+            } 
+            else
+            {
+                RemoveSelected.Visible = false; // The button for removing a selected item is set to be invisible
+                EditSelected.Visible = false; // The button for editting a selected item is set to be invisible
+                ItemNameEditorTextBox.Visible = false; // The textbox for editting an item's name is set to be invisible
+                ItemStockEditorTextBox.Visible = false; // The textbox for editting an item's stock is set to be invisible
+                ItemPriceEditorTextBox.Visible = false; // The textbox for editting an item's price is set to be invisible
+                AddStockButton.Visible = false; // The button for adding to an item's stock is set to be invisible
+                AddStockTextBox.Visible = false; // The textbox for adding to an item's stock is set to be invisible
+            }
+        }
+
+        // This code runs when the RemoveSelected button is clicked. It removes the selected item from the manager object and updates the InventoryDisplay listbox
+        private void RemoveSelected_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = InventoryDisplay.SelectedIndex; // An integer variable that stores the selected index of the InventoryDisplay object at the start of the method
+            manager.RemoveItem(manager.Items[InventoryDisplay.SelectedIndex - 1]); // The item selected is removed from the manager object
+            InventoryDisplay.Items.Clear(); // The InventoryDisplay listbox is cleared
+            InventoryDisplay.Items.Add(LISTBOXHEADER); // The LISTBOXHEADER string is added to the InventoryDisplay listbox
+            InventoryDisplay.Items.AddRange(manager.Display().ToArray()); // Calls the manager object's Display method, sets the result to an array, and adds the result to the InventoryDisplay ListBox
+            InventoryDisplay.SelectedIndex = selectedIndex - 1; // The selected index of the InventoryDisplay listbox is set to the selectedIndex variable minus one
+            if(selectedIndex - 1 == 0) // If the selectedIndex variable minus one equals zero, then the item in the InventoryDisplay listbox is deselected
+            {
+                InventoryDisplay.SelectedIndex = -1;
+            }
+        }
+
+        // This code runs when the EditSelected button is clicked. It changes the ItemName, Stock and Price of the selected InventoryItem based on data entered in the textboxes
+        private void EditSelected_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                manager.Items[InventoryDisplay.SelectedIndex - 1].ItemName = ItemNameEditorTextBox.Text; // The name of the selected InventoryItem is set to the data entered in the textbox
+                manager.RestockItem(manager.Items[InventoryDisplay.SelectedIndex - 1], int.Parse(ItemStockEditorTextBox.Text) - manager.Items[InventoryDisplay.SelectedIndex - 1].Stock); // The stock of the selected InventoryItem is set to the data entered in the textbox
+                manager.Items[InventoryDisplay.SelectedIndex - 1].Price = double.Parse(ItemPriceEditorTextBox.Text);  // The price of the selected InventoryItem is set to the data entered in the textbox
+                InventoryDisplay.Items.Clear(); // The InventoryDisplay listbox is cleared
+                InventoryDisplay.Items.Add(LISTBOXHEADER); // The LISTBOXHEADER string is added to the InventoryDisplay listbox
+                InventoryDisplay.Items.AddRange(manager.Display().ToArray()); // Calls the manager object's Display method, sets the result to an array, and adds the result to the InventoryDisplay ListBox
+
+                // The textboxes are cleared
+                ItemNameEditorTextBox.Text = "";
+                ItemStockEditorTextBox.Text = "";
+                ItemPriceEditorTextBox.Text = "";
+
+                RemoveSelected.Visible = false; // The button for removing a selected item is set to be invisible
+                EditSelected.Visible = false; // The button for editting a selected item is set to be invisible
+                ItemNameEditorTextBox.Visible = false; // The textbox for editting an item's name is set to be invisible
+                ItemStockEditorTextBox.Visible = false; // The textbox for editting an item's stock is set to be invisible
+                ItemPriceEditorTextBox.Visible = false; // The textbox for editting an item's price is set to be invisible
+                AddStockButton.Visible = false; // The button for adding to an item's stock is set to be invisible
+                AddStockTextBox.Visible = false; // The textbox for adding to an item's stock is set to be invisible
+            }
+            catch(Exception ex) // If any errors occur when parsing data from the textboxes, a message box containing the error message is displayed
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        // This code runs when the AddStockButton is clicked. It adds the data entered in the textbox to the selected InventoryItem's stock
+        private void AddStockButton_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                manager.RestockItem(manager.Items[InventoryDisplay.SelectedIndex - 1], int.Parse(AddStockTextBox.Text)); // The manager object's RestockItem method is called with the selected InventoryItem and the data from the textbox as parameters
+                InventoryDisplay.Items.Clear(); // The InventoryDisplay listbox is cleared
+                InventoryDisplay.Items.Add(LISTBOXHEADER); // The LISTBOXHEADER string is added to the InventoryDisplay listbox
+                InventoryDisplay.Items.AddRange(manager.Display().ToArray()); // Calls the manager object's Display method, sets the result to an array, and adds the result to the InventoryDisplay ListBox
+            }
+            catch(Exception ex) // If any errors occur when parsing data from the textboxes, a message box containing the error message is displayed
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // This code runs when the text of the SearchBar textbox is changed. The manager object's Search method is called with the data entered in the textbox as a parameter and the results are displayed in the SearchResult listbox
+        private void SearchBar_TextChanged(object sender, EventArgs e)
+        {
+            List<int> indeces = new List<int>(); // A List of integers that will contain the output of the Search method
+            List<InventoryItem> items = new List<InventoryItem>(); // A List of InventoryItem objects that will contain the output of the Search method
+
+            SearchResults.Items.Clear(); // The SearchResults listbox is cleared
+
+            manager.Search(SearchBar.Text, out indeces, out items); // The manager object's Search method is called with the text of the SearchBar textbox, the indeces List, and the items List as parameters
+            SearchResults.Items.AddRange((from item in items select item.ItemName).ToArray<string>()); // LINQ is used to add the names of the found items to the SearchResults listbox
+        }
+
+        // This code runs when an item is selected in the SearchResults listbox. It selects the corresponding item in the InventoryDisplay listbox
+        private void SearchResults_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            for(int i = 0; i < manager.Items.Count; i++) // This loop iterates for every item in the manager object
+            {
+                if (manager.Items[i].ItemName == (string)SearchResults.SelectedItem) // If the current ItemName property of the current item is equal to the SelectItem of the SearchResults listbox, the SelectedIndex of the InventoryDisplay listbox is set to the index of the item in the manager object plus one
+                {
+                    InventoryDisplay.SelectedIndex = manager.Items.IndexOf(manager.Items[i]) + 1;
+                }
+            }
+            
+        }
+
+    }
+}
